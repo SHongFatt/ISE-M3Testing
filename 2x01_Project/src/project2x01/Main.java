d0ab3aea0f099b4f22852b48fc66d22c5ffc0d7d
sithongfatt <1702635@sit.singaporetech.edu.sg>
2018-11-06 00:31:23 +0800

    Added Junit class file
    added junit class file and removed not not using file

--- 2x01_Project/src/project2x01/Main.java
+++ 2x01_Project/src/project2x01/Main.java
@@ -6,9 +6,12 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("Hello");
+		//System.out.print("Hello");
 		AdminModel admin = new AdminModel();
-		admin.updateNumberOfVehicle(1, "");
+		//admin.updateNumberOfVehicle(1, "");
+		admin.updateNumberOfVehicle(1, "abc");
+		//System.out.println(admin.env.getlevelEnvNumOfVehicle());
+		
 	}
 
 }
